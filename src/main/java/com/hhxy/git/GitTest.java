fa8e73f1efbb8f2dd244b0bc6c11812efddba4a3
Xiaojie <877003469@qq.com>
2023-03-01 16:03:12 +0800

    hox fix第一次提交

--- src/main/java/com/hhxy/git/GitTest.java
+++ src/main/java/com/hhxy/git/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("Hello World!!!");
         System.out.println("Hello World!!!");
         System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
+        System.out.println("Hello World123123!!!");
     }
 }
